c3df2968de2d0fcbf8fdda2c4ca4da62c692b051
jbevain <jbevain@gmail.com>
2010-04-19 14:22:07 +0200

    bump version to 0.9.1

--- Test/Mono.Cecil.Tests/AssemblyInfo.cs
+++ Test/Mono.Cecil.Tests/AssemblyInfo.cs
@@ -2,35 +2,13 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle ("Mono.Cecil.Tests")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("Mono.Cecil.Tests")]
-[assembly: AssemblyCopyright ("Copyright ©  2008")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyProduct ("Mono.Cecil")]
+[assembly: AssemblyCopyright ("Copyright © 2008 - 2010 Jb Evain")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible (false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid ("da96c202-696a-457e-89af-5fa74e6bda0d")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion ("1.0.0.0")]
 [assembly: AssemblyFileVersion ("1.0.0.0")]
