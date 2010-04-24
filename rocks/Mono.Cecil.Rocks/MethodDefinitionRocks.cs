e1c9069b170f8f065617542d5ec73d250517763a
jbevain <jbevain@gmail.com>
2010-04-24 14:27:37 +0200

    implement GetBaseMethod

--- rocks/Mono.Cecil.Rocks/MethodDefinitionRocks.cs
+++ rocks/Mono.Cecil.Rocks/MethodDefinitionRocks.cs
@@ -39,9 +39,18 @@ namespace Mono.Cecil.Rocks {
 			if (self == null)
 				throw new ArgumentNullException ("self");
 			if (!self.IsVirtual)
-				return null;
+				return self;
 
-			throw new NotImplementedException ();
+			var base_type = ResolveBaseType (self.DeclaringType);
+			while (base_type != null) {
+				var @base = GetMatchingMethod (base_type, self);
+				if (@base != null)
+					return @base;
+
+				base_type = ResolveBaseType (base_type);
+			}
+
+			return self;
 		}
 
 		public static MethodDefinition GetOriginalBaseMethod (this MethodDefinition self)
@@ -49,7 +58,30 @@ namespace Mono.Cecil.Rocks {
 			if (self == null)
 				throw new ArgumentNullException ("self");
 
-			throw new NotImplementedException ();
+			while (true) {
+				var @base = self.GetBaseMethod ();
+				if (@base == self)
+					return self;
+
+				self = @base;
+			}
+		}
+
+		static TypeDefinition ResolveBaseType (TypeDefinition type)
+		{
+			if (type == null)
+				return null;
+
+			var base_type = type.BaseType;
+			if (base_type == null)
+				return null;
+
+			return base_type.Resolve ();
+		}
+
+		static MethodDefinition GetMatchingMethod (TypeDefinition type, MethodDefinition method)
+		{
+			return MetadataResolver.GetMethod (type.Methods, method);
 		}
 	}
 }
