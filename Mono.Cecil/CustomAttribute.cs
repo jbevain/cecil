6069ed954aa1907b6043a67e01eaa651ef94761b
jbevain <jbevain@gmail.com>
2010-05-25 12:33:05 +0200

    cosmetic

--- Mono.Cecil/CustomAttribute.cs
+++ Mono.Cecil/CustomAttribute.cs
@@ -75,7 +75,9 @@ namespace Mono.Cecil {
 	}
 
 	public interface ICustomAttribute {
+
 		TypeReference AttributeType { get; }
+
 		Collection<CustomAttributeNamedArgument> Fields { get; }
 		Collection<CustomAttributeNamedArgument> Properties { get; }
 	}
