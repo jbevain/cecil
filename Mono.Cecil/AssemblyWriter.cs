a98dacddaabc96fc64b6eb4d267156ab3df73f26
Jb Evain <jbevain@gmail.com>
2010-06-07 14:21:48 +0200

    set the strong named signed flag on the module whenever it has a public key

--- Mono.Cecil/AssemblyWriter.cs
+++ Mono.Cecil/AssemblyWriter.cs
@@ -89,19 +89,21 @@ namespace Mono.Cecil {
 
 			module.MetadataSystem.Clear ();
 
+			var name = module.assembly.Name;
 			var fq_name = stream.GetFullyQualifiedName ();
 			var symbol_writer_provider = parameters.SymbolWriterProvider;
 			if (symbol_writer_provider == null && parameters.WriteSymbols)
 				symbol_writer_provider = SymbolProvider.GetPlatformWriterProvider ();
 			var symbol_writer = GetSymbolWriter (module, fq_name, symbol_writer_provider);
+
 #if !SILVERLIGHT && !CF
-			if (parameters.StrongNameKeyPair != null && module.assembly != null) {
-				var name = module.assembly.Name;
+			if (parameters.StrongNameKeyPair != null && module.assembly != null)
 				name.PublicKey = parameters.StrongNameKeyPair.PublicKey;
-				name.HasPublicKey = true;
-				module.Attributes |= ModuleAttributes.StrongNameSigned;
-			}
 #endif
+
+			if (name.HasPublicKey)
+				module.Attributes |= ModuleAttributes.StrongNameSigned;
+
 			var metadata = new MetadataBuilder (module, fq_name,
 				symbol_writer_provider, symbol_writer);
 
