9469f162e06163eb968ee5939f54c7398dff0ecb
jbevain <jbevain@gmail.com>
2010-07-04 12:56:03 +0200

    add HasFields and HasProperties to ICustomAttribute

--- Mono.Cecil/CustomAttribute.cs
+++ Mono.Cecil/CustomAttribute.cs
@@ -78,6 +78,8 @@ namespace Mono.Cecil {
 
 		TypeReference AttributeType { get; }
 
+		bool HasFields { get; }
+		bool HasProperties { get; }
 		Collection<CustomAttributeNamedArgument> Fields { get; }
 		Collection<CustomAttributeNamedArgument> Properties { get; }
 	}
