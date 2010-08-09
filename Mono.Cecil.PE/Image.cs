8db5ca4e78ec5475884c00c94dc4cdd70ed9f48e
jbevain <jbevain@gmail.com>
2010-08-09 11:55:00 +0200

    compute the debug header position using direct pointers instead of rvas as SRE doesn't populate those fields

--- Mono.Cecil.PE/Image.cs
+++ Mono.Cecil.PE/Image.cs
@@ -148,7 +148,7 @@ namespace Mono.Cecil.PE {
 				PointerToRawData = buffer.ReadInt32 (),
 			};
 
-			buffer.position = (int) (directory.AddressOfRawData - section.VirtualAddress);
+			buffer.position = (int) (directory.PointerToRawData - section.PointerToRawData);
 
 			header = new byte [directory.SizeOfData];
 			Buffer.BlockCopy (buffer.buffer, buffer.position, header, 0, header.Length);
