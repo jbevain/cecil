ec89dc59897baeef5d9069eb497af3739dd06442
jbevain <jbevain@gmail.com>
2011-04-07 09:59:01 +0200

    Add Action for net_2_0

--- Mono/Actions.cs
+++ Mono/Actions.cs
@@ -30,7 +30,7 @@
 
 namespace Mono {
 	//delegate void Action ();
-	//delegate void Action<T1, T2> (T1 arg1, T2 arg2);
+	delegate void Action<T1, T2> (T1 arg1, T2 arg2);
 	//delegate void Action<T1, T2, T3> (T1 arg1, T2 arg2, T3 arg3);
 	//delegate void Action<T1, T2, T3, T4> (T1 arg1, T2 arg2, T3 arg3, T4 arg4);
 }
