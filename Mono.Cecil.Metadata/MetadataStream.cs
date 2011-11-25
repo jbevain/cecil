020013c343fabafef03bb65fdb77d39b79213bbf
de4dot <de4dot@gmail.com>
2011-11-25 15:10:29 +0100

    First char should be upper case

--- Mono.Cecil.Metadata/MetadataStream.cs
+++ Mono.Cecil.Metadata/MetadataStream.cs
@@ -3,11 +3,11 @@
 namespace Mono.Cecil.Metadata {
 	public sealed class MetadataStream : Heap {
 
-		public string name;
+		public string Name;
 
 		public MetadataStream (Section section, uint start, uint size, string name)
 			: base (section, start, size) {
-			this.name = name;
+			this.Name = name;
 		}
 	}
 }
