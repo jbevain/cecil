jbevain/cecil

commit 4de92b776d9c0739347c5bfb4288ee7a86c6e818

Unescape a type AssemblyQualifiedName if needed, ref: http://msdn.microsoft.com/en-us/library/system.type.assemblyqualifiedname.aspx

Mono.Cecil/AssemblyReader.cs
@@ -3099,9 +3099,29 @@ namespace Mono.Cecil {
 			}
 		}
 
+		string UnescapeTypeName (string name)
+		{
+			StringBuilder sb = new StringBuilder (name.Length);
+			for (int i = 0; i < name.Length; i++) {
+				char c = name [i];
+				if (name [i] == '\\') {
+					if ((i < name.Length - 1) && (name [i + 1] == '\\')) {
+						sb.Append (c);
+						i++;
+					}
+				} else {
+					sb.Append (c);
+				}
+			}
+			return sb.ToString ();
+		}
+
 		public TypeReference ReadTypeReference ()
 		{
-			return TypeParser.ParseType (reader.module, ReadUTF8String ());
+			string s = ReadUTF8String ();
+			if (s.IndexOf ('\\') != -1)
+				s = UnescapeTypeName (s);
+			return TypeParser.ParseType (reader.module, s);
 		}
 
 		object ReadCustomAttributeEnum (TypeReference enum_type)

commit 3fd44ba68ffcb89117db89a601134eef65ef7f24

Add a null-check to fix a regression in 4de92b7.
The regression (NRE) occurs if null is passed as the argument to an
attribute constructor taking a type:

    [SomeAttribute (null)]

Mono.Cecil/AssemblyReader.cs
@@ -3119,7 +3119,7 @@ namespace Mono.Cecil {
 		public TypeReference ReadTypeReference ()
 		{
 			string s = ReadUTF8String ();
-			if (s.IndexOf ('\\') != -1)
+			if (s != null && s.IndexOf ('\\') != -1)
 				s = UnescapeTypeName (s);
 			return TypeParser.ParseType (reader.module, s);
 		}