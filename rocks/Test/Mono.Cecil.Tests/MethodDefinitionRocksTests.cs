41f3bc703931e782ac87576062ec3938aa8a01c2
Jb Evain <jbevain@gmail.com>
2014-11-02 12:08:25 +0100

    Properly handle newslot in GetBaseMethod

--- rocks/Test/Mono.Cecil.Tests/MethodDefinitionRocksTests.cs
+++ rocks/Test/Mono.Cecil.Tests/MethodDefinitionRocksTests.cs
@@ -11,16 +11,25 @@ namespace Mono.Cecil.Tests {
 
 		abstract class Foo {
 			public abstract void DoFoo ();
+			public abstract void DoBar ();
 		}
 
 		class Bar : Foo {
 			public override void DoFoo ()
 			{
 			}
+
+			public override void DoBar ()
+			{
+			}
 		}
 
 		class Baz : Bar {
 			public override void DoFoo ()
+			{
+			}
+
+			public virtual new void DoBar ()
 			{
 			}
 		}
@@ -38,6 +47,10 @@ namespace Mono.Cecil.Tests {
 			Assert.AreEqual ("Foo", @base.DeclaringType.Name);
 
 			Assert.AreEqual (@base, @base.GetBaseMethod ());
+
+			var new_dobar = baz.GetMethod ("DoBar");
+			@base = new_dobar.GetBaseMethod();
+			Assert.AreEqual("Baz", @base.DeclaringType.Name);
 		}
 
 		[Test]
