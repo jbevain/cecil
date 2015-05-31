4dd216657d689d10f8c98962fdad7775f6473a25
Francois Valdy <francois.valdy@gmail.com>
2015-05-31 12:22:34 +0200

    Remove unnecessary clear call
    - already done in Shift

--- Mono.Collections.Generic/Collection.cs
+++ Mono.Collections.Generic/Collection.cs
@@ -169,7 +169,6 @@ namespace Mono.Collections.Generic {
 			OnRemove (item, index);
 
 			Shift (index, -1);
-			Array.Clear (items, size, 1);
 			version++;
 		}
 
@@ -182,7 +181,6 @@ namespace Mono.Collections.Generic {
 			OnRemove (item, index);
 
 			Shift (index, -1);
-			Array.Clear (items, size, 1);
 			version++;
 
 			return true;
