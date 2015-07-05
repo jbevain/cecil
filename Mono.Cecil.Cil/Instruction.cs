33fdee08359bc5dd299f4097c38308700c4f0a7e
Jason Holloway <miceeatnicerice@hotmail.com>
2015-07-05 15:39:48 +0100

    Fix opcode checks when emitting byte constants

--- Mono.Cecil.Cil/Instruction.cs
+++ Mono.Cecil.Cil/Instruction.cs
@@ -209,7 +209,7 @@ namespace Mono.Cecil.Cil {
 
 		public static Instruction Create (OpCode opcode, sbyte value)
 		{
-			if (opcode.OperandType != OperandType.ShortInlineI &&
+			if (opcode.OperandType != OperandType.ShortInlineI ||
 				opcode != OpCodes.Ldc_I4_S)
 				throw new ArgumentException ("opcode");
 
@@ -219,7 +219,7 @@ namespace Mono.Cecil.Cil {
 		public static Instruction Create (OpCode opcode, byte value)
 		{
 			if (opcode.OperandType != OperandType.ShortInlineI ||
-				opcode == OpCodes.Ldc_I4_S)
+				opcode != OpCodes.Ldc_I4_S)
 				throw new ArgumentException ("opcode");
 
 			return new Instruction (opcode, value);
