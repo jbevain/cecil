46356a5611623a3c32dea5f5b948c771f6764e3a
Jb Evain <jb@evain.net>
2016-07-23 11:09:13 -0700

    Bump Cecil version

--- ProjectInfo.cs
+++ ProjectInfo.cs
@@ -17,5 +17,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible (false)]
 #endif
 
-[assembly: AssemblyVersion ("0.9.6.0")]
-[assembly: AssemblyFileVersion ("0.9.6.0")]
+[assembly: AssemblyVersion ("0.10.0.0")]
+[assembly: AssemblyFileVersion ("0.10.0.0")]
