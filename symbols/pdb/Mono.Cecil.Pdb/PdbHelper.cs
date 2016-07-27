ec15879a364b65826eb8a37866a991d50f2a569c
Jb Evain <jb@evain.net>
2016-07-26 20:37:52 -0700

    Remove dead code

--- symbols/pdb/Mono.Cecil.Pdb/PdbHelper.cs
+++ symbols/pdb/Mono.Cecil.Pdb/PdbHelper.cs
@@ -68,14 +68,3 @@ namespace Mono.Cecil.Pdb {
 
 #endif
 }
-
-#if !NET_3_5 && !NET_4_0
-
-namespace System.Runtime.CompilerServices {
-
-	[AttributeUsage (AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
-	sealed class ExtensionAttribute : Attribute {
-	}
-}
-
-#endif
