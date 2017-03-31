ad363180819ef7b996ec5d86cfeff40f42bcb3a8
Josh Peterson <petersonjm1@gmail.com>
2017-03-31 13:31:51 -0400

    Do not project for windows runtime during read.
    A type will not be fully initialized during read, so don't attempt to
    project for Windows runtime until after all of the metadata reading is
    correct.
    
    If a type reading for a module is no completed, a call to
    BinaryRangeSearch in MetadataSystem can fail, because some of the
    entries in the types array will still be null.

--- Mono.Cecil/AssemblyReader.cs
+++ Mono.Cecil/AssemblyReader.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 // Author:
 //   Jb Evain (jbevain@gmail.com)
 //
@@ -845,6 +845,12 @@ namespace Mono.Cecil {
 
 				types [i] = ReadType (i + 1);
 			}
+
+			if (module.IsWindowsMetadata()) {
+				for (uint i = 0; i < length; i++) {
+					WindowsRuntimeProjections.Project (types [i]);
+				}
+			}
 		}
 
 		static bool IsNested (TypeAttributes attributes)
@@ -956,9 +962,6 @@ namespace Mono.Cecil {
 			if (IsNested (attributes))
 				type.DeclaringType = GetNestedTypeDeclaringType (type);
 
-			if (module.IsWindowsMetadata ())
-				WindowsRuntimeProjections.Project (type);
-
 			return type;
 		}
 
@@ -1053,7 +1056,13 @@ namespace Mono.Cecil {
 			if (type != null)
 				return type;
 
-			return ReadTypeDefinition (rid);
+			type = ReadTypeDefinition (rid);
+
+			if (module.IsWindowsMetadata()) {
+				WindowsRuntimeProjections.Project (type);
+			}
+
+			return type;
 		}
 
 		TypeDefinition ReadTypeDefinition (uint rid)
