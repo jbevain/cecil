6c8b4c3f3a2929f683e6fdb41ebc825c7e73b8e7
Jb Evain <jb@evain.net>
2017-05-16 23:16:24 -0700

    Add test

--- symbols/pdb/Test/Mono.Cecil.Tests/PdbTests.cs
+++ symbols/pdb/Test/Mono.Cecil.Tests/PdbTests.cs
@@ -359,6 +359,42 @@ namespace Mono.Cecil.Tests {
 		}
 
 		[Test]
+		public void ImportsForFirstMethod ()
+		{
+			TestModule ("CecilTest.exe", module => {
+				var type = module.GetType ("CecilTest.Program");
+				var method = type.GetMethod ("Main");
+
+				var debug = method.DebugInformation;
+				var scope = debug.Scope;
+
+				Assert.IsTrue (scope.End.IsEndOfMethod);
+
+				var import = scope.Import;
+
+				Assert.IsNotNull (import);
+				Assert.AreEqual (5, import.Targets.Count);
+
+				var ns = new [] {
+					"System",
+					"System.Collections.Generic",
+					"System.Linq",
+					"System.Text",
+					"System.Threading.Tasks",
+				};
+
+				for (int i = 0; i < import.Targets.Count; i++) {
+					var target = import.Targets [i];
+
+					Assert.AreEqual (ImportTargetKind.ImportNamespace, target.Kind);
+					Assert.AreEqual (ns [i], target.Namespace);
+				}
+
+				Assert.AreEqual ("System", import.Targets [0].Namespace);
+			}, readOnly: Platform.OnMono, symbolReaderProvider: typeof (PdbReaderProvider), symbolWriterProvider: typeof (PdbWriterProvider));
+		}
+
+		[Test]
 		public void CreateMethodFromScratch ()
 		{
 			IgnoreOnMono ();
