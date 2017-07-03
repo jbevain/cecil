jbevain/cecil

commit 7c8e0f767d7b2f652a430e4e26f1d98a20f9e125

Fix reading end of scope symbols

Mono.Cecil.Cil/CodeReader.cs
@@ -160,12 +160,12 @@ namespace Mono.Cecil.Cil {
 		void ReadScope (ScopeDebugInformation scope)
 		{
 			var start_instruction = GetInstruction (scope.Start.Offset);
-			if (start_instruction != null)
-				scope.Start = new InstructionOffset (start_instruction);
+			scope.Start = new InstructionOffset (start_instruction);
 
 			var end_instruction = GetInstruction (scope.End.Offset);
-			if (end_instruction != null)
-				scope.End = new InstructionOffset (end_instruction);
+			scope.End = end_instruction != null
+				? new InstructionOffset (end_instruction)
+				: new InstructionOffset ();
 
 			if (!scope.variables.IsNullOrEmpty ()) {
 				for (int i = 0; i < scope.variables.Count; i++) {

commit 44e4d344d674195b4d2950c48fd1cb55fe42a1ef

fix reading scope start
 - in case when the mdb file is out of sync, we might endup with
   start_instruction being null

 - it is regression introduced by
   7c8e0f767d7b2f652a430e4e26f1d98a20f9e125

Mono.Cecil.Cil/CodeReader.cs
@@ -160,7 +160,8 @@ namespace Mono.Cecil.Cil {
 		void ReadScope (ScopeDebugInformation scope)
 		{
 			var start_instruction = GetInstruction (scope.Start.Offset);
-			scope.Start = new InstructionOffset (start_instruction);
+			if (start_instruction != null)
+				    scope.Start = new InstructionOffset (start_instruction);
 
 			var end_instruction = GetInstruction (scope.End.Offset);
 			scope.End = end_instruction != null