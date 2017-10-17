df0355e147687cfd5dcd339e29e07b79c151917c
Flávio <flaviok@stone.com.br>
2017-10-17 21:35:30 -0200

    Migrated product information to file 'Directory.Build.props'.

--- src/Mono.Cecil/AssemblyInfo.cs
+++ src/Mono.Cecil/AssemblyInfo.cs
@@ -12,8 +12,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle (Consts.AssemblyName)]
-
 #if !NET_CORE
 [assembly: Guid ("fd225bb4-fa53-44b2-a6db-85f5e48dcb54")]
 #endif
