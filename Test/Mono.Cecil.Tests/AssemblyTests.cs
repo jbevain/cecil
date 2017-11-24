36117614ddd66b62184e7148aa85c25a57193ce7
Chris Hamons <chris.hamons@xamarin.com>
2017-11-24 17:00:28 -0600

    Add test for mismatched pdb (#469)
    * Add test for mismatched pdb
    
    - Test for 023812807c0cb0dfe5bb5752df17b8ec0bf5994b
    
    * Code review changes
    
    * More code review changes
    
    * Fix csproj

--- Test/Mono.Cecil.Tests/AssemblyTests.cs
+++ Test/Mono.Cecil.Tests/AssemblyTests.cs
@@ -89,5 +89,19 @@ namespace Mono.Cecil.Tests {
 				Assert.AreEqual (module.AssemblyReferences [0], module.TypeSystem.CoreLibrary);
 			}, verify: !Platform.OnMono);
 		}
+
+		[Test]
+		public void MismatchedLibraryAndSymbols_DoNotThrow ()
+		{
+			// SQLite-net.dll (from nuget) shiped with mismatched symbol files, but throwIfNoSymbol did not prevent it from throwing
+			var readerParms = new ReaderParameters {
+				ReadSymbols = true,
+				SymbolReaderProvider = new Cil.DefaultSymbolReaderProvider (throwIfNoSymbol: false)
+			};
+
+			using (var module = GetResourceModule ("SQLite-net.dll", readerParms)) {
+				Assert.Null (module.SymbolReader);
+			}
+		}
 	}
 }
