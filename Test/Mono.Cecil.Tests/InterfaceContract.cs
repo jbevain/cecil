6fe73594ab160dfc598e3fd67dff50c117c040f8
Steve Gilham <SteveGilham@users.noreply.github.com>
2018-03-22 14:31:39 +0000

    Record the assembly version that went with the failure

--- Test/Mono.Cecil.Tests/InterfaceContract.cs
+++ Test/Mono.Cecil.Tests/InterfaceContract.cs
@@ -22,7 +22,6 @@ namespace Mono.Cecil.Tests {
 			var cecilDll = typeof (Mono.TypeExtensions).Assembly.Location;
 			var assembly = AssemblyDefinition.ReadAssembly (cecilDll);
 			var version = assembly.Name;
-			Console.WriteLine (version);
 			var visibleTypes = assembly.Modules
 				.SelectMany (m => m.Types)
 				.SelectMany (GetNestedTypes)
@@ -68,12 +67,16 @@ namespace Mono.Cecil.Tests {
 			Array.ForEach (signature,
 				i => str.AppendFormat ("{0:X2}", i));
 			var result = str.ToString ();
+
+			// for version 0.10.0-beta7 'Mono.Cecil, Version=0.10.0.0, Culture=neutral, PublicKeyToken=50cebf1cceb9d05e'
 			var expected = "73749C57E84195F3998A7BE05EB137E74D60A545731479D3FF8D00FDAF35FAE2";
 
 			Assert.That (result, Is.EqualTo (expected),
 				"The interface signature has changed and is now '" +
 				result + "'.  If there has been a public release, " +
-				"increment the assembly version to reflect this fact," +
+				"increment the assembly version from '" +
+				version.FullName +
+				"' to reflect this fact," +
 				"then rerun this test and record the new value from this failure." +
 				"Update 'expected' with that value, and verify that the test is now clean."
 				);
