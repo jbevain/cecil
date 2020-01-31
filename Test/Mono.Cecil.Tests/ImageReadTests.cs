40aaba7b8d188366c57c074c9a65303684269d59
Jb Evain <jbevain@syntaxtree.com>
2020-01-31 08:57:43 -0800

    Style fixes

--- Test/Mono.Cecil.Tests/ImageReadTests.cs
+++ Test/Mono.Cecil.Tests/ImageReadTests.cs
@@ -212,13 +212,13 @@ namespace Mono.Cecil.Tests {
 		{
 			TestModule ("LocallyScopedConstantArray.dll", module => {
 				Assert.IsTrue (module.HasDebugHeader);
-				var method= module.Types
+				var method = module.Types
 					.Single (x => x.Name == "TestClass")
 					.Methods
 					.Single (x => x.Name == "TestMethod");
 				var debugInformation = method.DebugInformation;
-				Assert.IsNull(debugInformation.Scope.Constants.Single().Value);
-			}, symbolReaderProvider: typeof(PortablePdbReaderProvider), symbolWriterProvider: typeof(PortablePdbWriterProvider));
+				Assert.IsNull (debugInformation.Scope.Constants.Single ().Value);
+			}, symbolReaderProvider: typeof (PortablePdbReaderProvider), symbolWriterProvider: typeof (PortablePdbWriterProvider));
 		}
 
 		[Test]
