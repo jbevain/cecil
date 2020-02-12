1a0b9fca321b5f0b77ce987917579e136527461b
Thays Grazia <thaystg@gmail.com>
2020-02-12 19:42:17 -0300

    Removing test infrastructure.

--- Test/Mono.Cecil.Tests/PortablePdbTests.cs
+++ Test/Mono.Cecil.Tests/PortablePdbTests.cs
@@ -372,22 +372,27 @@ namespace Mono.Cecil.Tests {
 		[Test]
 		public void EmbeddedCompressedPortablePdbFromStream ()
 		{
-			TestBinary("EmbeddedCompressedPdbTarget.exe", module => {
-				Assert.IsTrue (module.HasDebugHeader);
+			var bytes = File.ReadAllBytes (GetAssemblyResourcePath ("EmbeddedCompressedPdbTarget.exe"));
+			var parameters = new ReaderParameters {
+				ReadSymbols = true,
+				SymbolReaderProvider = new PdbReaderProvider ()
+			};
 
-				var header = module.GetDebugHeader ();
+			var module = ModuleDefinition.ReadModule (new MemoryStream(bytes), parameters);
+			Assert.IsTrue (module.HasDebugHeader);
 
-				Assert.IsNotNull (header);
-				Assert.AreEqual (2, header.Entries.Length);
+			var header = module.GetDebugHeader ();
 
-				var cv = header.Entries [0];
-				Assert.AreEqual (ImageDebugType.CodeView, cv.Directory.Type);
+			Assert.IsNotNull (header);
+			Assert.AreEqual (2, header.Entries.Length);
 
-				var eppdb = header.Entries [1];
-				Assert.AreEqual (ImageDebugType.EmbeddedPortablePdb, eppdb.Directory.Type);
-				Assert.AreEqual (0x0100, eppdb.Directory.MajorVersion);
-				Assert.AreEqual (0x0100, eppdb.Directory.MinorVersion);
-			}, symbolReaderProvider: typeof (PdbReaderProvider), symbolWriterProvider: null);
+			var cv = header.Entries [0];
+			Assert.AreEqual (ImageDebugType.CodeView, cv.Directory.Type);
+
+			var eppdb = header.Entries [1];
+			Assert.AreEqual (ImageDebugType.EmbeddedPortablePdb, eppdb.Directory.Type);
+			Assert.AreEqual (0x0100, eppdb.Directory.MajorVersion);
+			Assert.AreEqual (0x0100, eppdb.Directory.MinorVersion);
 		}
 
 
