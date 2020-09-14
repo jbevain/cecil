a4c20ac8e80a8e5ca1b7283c481c9d5201726ad3
Tautvydas Žilys <tautvydas.zilys@gmail.com>
2020-09-14 12:27:15 -0700

    Fix code formatting.

--- Test/Mono.Cecil.Tests/MethodReferenceComparerTests.cs
+++ Test/Mono.Cecil.Tests/MethodReferenceComparerTests.cs
@@ -2,134 +2,134 @@ using NUnit.Framework;
 using System;
 using System.Linq;
 
-namespace Mono.Cecil.Tests
-{
-    [TestFixture]
-    public class MethodReferenceComparerTests : LoadAssemblyDefinitionForTestsBaseSimple
-    {
-        private TypeDefinition _class1;
-        private TypeDefinition _class2;
+namespace Mono.Cecil.Tests {
 
-        [SetUp]
-        public void SetUp()
-        {
-            SetupAssemblyDefinitions(typeof(MethodReferenceComparerTests).Assembly);
-            _class1 = TypeDefinitionUtils.TypeDefinitionFor(typeof(Class1), _assembly);
-            _class2 = TypeDefinitionUtils.TypeDefinitionFor(typeof(Class2), _assembly);
-        }
+	[TestFixture]
+	public class MethodReferenceComparerTests : LoadAssemblyDefinitionForTestsBaseSimple {
 
-        [Test]
-        public void MethodReferenceEqualsMethodDefinition()
-        {
-            var typeDefinition = TypeDefinitionUtils.TypeDefinitionFor(typeof(Int32), _mscorlib);
-            var method = typeDefinition.Methods.Single(m => m.Name == "GetHashCode");
-            var methodReference = new MethodReference(method.Name, method.ReturnType, method.DeclaringType);
-            methodReference.HasThis = method.HasThis;
+		private TypeDefinition _class1;
+		private TypeDefinition _class2;
 
-            Assert.That(MethodReferenceComparer.AreEqual(method, methodReference), Is.True);
-        }
+		[SetUp]
+		public void SetUp ()
+		{
+			SetupAssemblyDefinitions (typeof (MethodReferenceComparerTests).Assembly);
+			_class1 = TypeDefinitionUtils.TypeDefinitionFor (typeof (Class1), _assembly);
+			_class2 = TypeDefinitionUtils.TypeDefinitionFor (typeof (Class2), _assembly);
+		}
 
-        [Test]
-        public void VerifyMethodSignatureMatches()
-        {
-            Assert.IsTrue(CompareSignatures("MethodWithNoParametersOrReturn"));
-            Assert.IsTrue(CompareSignatures("GenericMethodWithNoParametersOrReturn"));
-            Assert.IsFalse(CompareSignatures("MethodWithNoParametersOrReturn", "GenericMethodWithNoParametersOrReturn"));
+		[Test]
+		public void MethodReferenceEqualsMethodDefinition ()
+		{
+			var typeDefinition = TypeDefinitionUtils.TypeDefinitionFor (typeof (Int32), _mscorlib);
+			var method = typeDefinition.Methods.Single (m => m.Name == "GetHashCode");
+			var methodReference = new MethodReference (method.Name, method.ReturnType, method.DeclaringType);
+			methodReference.HasThis = method.HasThis;
 
-            Assert.IsTrue(CompareSignatures("MethodWithIntParameterAndVoidReturn"));
-        }
+			Assert.That (MethodReferenceComparer.AreEqual (method, methodReference), Is.True);
+		}
 
-        [Test]
-        public void VerifySignatureComparisonConsidersStatic()
-        {
-            Assert.IsTrue(CompareSignatures("StaticMethodWithNoParametersOrReturn"));
-            Assert.IsTrue(CompareSignatures("StaticMethodWithNoParametersOrReturn"));
-            Assert.IsFalse(CompareSignatures("MethodWithNoParametersOrReturn", "StaticMethodWithNoParametersOrReturn"));
-            Assert.IsFalse(CompareSignatures("GenericMethodWithNoParametersOrReturn", "GenericStaticMethodWithNoParametersOrReturn"));
-        }
+		[Test]
+		public void VerifyMethodSignatureMatches ()
+		{
+			Assert.IsTrue (CompareSignatures ("MethodWithNoParametersOrReturn"));
+			Assert.IsTrue (CompareSignatures ("GenericMethodWithNoParametersOrReturn"));
+			Assert.IsFalse (CompareSignatures ("MethodWithNoParametersOrReturn", "GenericMethodWithNoParametersOrReturn"));
 
-        [Test]
-        public void VerifyMethodSignatureWithGenericParameters()
-        {
-            Assert.IsTrue(CompareSignatures("GenericMethodWithGenericParameter"));
-            Assert.IsTrue(CompareSignatures("GenericMethodWithGenericParameterArray"));
-            Assert.IsTrue(CompareSignatures("GenericMethodWithByReferenceGenericParameter"));
-            Assert.IsTrue(CompareSignatures("GenericMethodWithGenericInstanceGenericParameter"));
-        }
+			Assert.IsTrue (CompareSignatures ("MethodWithIntParameterAndVoidReturn"));
+		}
 
-        [Test]
-        public void VerifyNonResolvableMethodReferencesWithDifferentParameterTypesAreNotEqual()
-        {
-            var method1 = new MethodReference("TestMethod", _class1.Module.TypeSystem.Void, _class1);
-            method1.Parameters.Add(new ParameterDefinition(new ByReferenceType(_class1.Module.TypeSystem.Int16)));
+		[Test]
+		public void VerifySignatureComparisonConsidersStatic ()
+		{
+			Assert.IsTrue (CompareSignatures ("StaticMethodWithNoParametersOrReturn"));
+			Assert.IsTrue (CompareSignatures ("StaticMethodWithNoParametersOrReturn"));
+			Assert.IsFalse (CompareSignatures ("MethodWithNoParametersOrReturn", "StaticMethodWithNoParametersOrReturn"));
+			Assert.IsFalse (CompareSignatures ("GenericMethodWithNoParametersOrReturn", "GenericStaticMethodWithNoParametersOrReturn"));
+		}
 
-            var method2 = new MethodReference("TestMethod", _class1.Module.TypeSystem.Void, _class1);
-            method2.Parameters.Add(new ParameterDefinition(new ByReferenceType(_class1.Module.TypeSystem.Char)));
+		[Test]
+		public void VerifyMethodSignatureWithGenericParameters ()
+		{
+			Assert.IsTrue (CompareSignatures ("GenericMethodWithGenericParameter"));
+			Assert.IsTrue (CompareSignatures ("GenericMethodWithGenericParameterArray"));
+			Assert.IsTrue (CompareSignatures ("GenericMethodWithByReferenceGenericParameter"));
+			Assert.IsTrue (CompareSignatures ("GenericMethodWithGenericInstanceGenericParameter"));
+		}
 
-            Assert.IsFalse(MethodReferenceComparer.AreEqual(method1, method2));
-        }
+		[Test]
+		public void VerifyNonResolvableMethodReferencesWithDifferentParameterTypesAreNotEqual ()
+		{
+			var method1 = new MethodReference ("TestMethod", _class1.Module.TypeSystem.Void, _class1);
+			method1.Parameters.Add (new ParameterDefinition (new ByReferenceType (_class1.Module.TypeSystem.Int16)));
 
-        [Test]
-        public void VerifyNonResolvableRecursiveMethodsDontStackOverflow()
-        {
-            var method1 = new MethodReference("TestMethod", _class1.Module.TypeSystem.Void, _class1);
-            method1.GenericParameters.Add(new GenericParameter(method1));
-            method1.Parameters.Add(new ParameterDefinition(method1.GenericParameters[0]));
+			var method2 = new MethodReference ("TestMethod", _class1.Module.TypeSystem.Void, _class1);
+			method2.Parameters.Add (new ParameterDefinition (new ByReferenceType (_class1.Module.TypeSystem.Char)));
 
-            var method2 = new MethodReference("TestMethod", _class1.Module.TypeSystem.Void, _class1);
-            method2.GenericParameters.Add(new GenericParameter(method2));
-            method2.Parameters.Add(new ParameterDefinition(method2.GenericParameters[0]));
+			Assert.IsFalse (MethodReferenceComparer.AreEqual (method1, method2));
+		}
 
-            Assert.IsTrue(MethodReferenceComparer.AreEqual(method1, method2));
-        }
+		[Test]
+		public void VerifyNonResolvableRecursiveMethodsDontStackOverflow ()
+		{
+			var method1 = new MethodReference ("TestMethod", _class1.Module.TypeSystem.Void, _class1);
+			method1.GenericParameters.Add (new GenericParameter (method1));
+			method1.Parameters.Add (new ParameterDefinition (method1.GenericParameters[0]));
 
-        bool CompareSignatures(string name)
-        {
-            return CompareSignatures(name, name);
-        }
+			var method2 = new MethodReference ("TestMethod", _class1.Module.TypeSystem.Void, _class1);
+			method2.GenericParameters.Add (new GenericParameter (method2));
+			method2.Parameters.Add (new ParameterDefinition (method2.GenericParameters[0]));
 
-        bool CompareSignatures(string name1, string name2)
-        {
-            return MethodReferenceComparer.AreSignaturesEqual(GetMethod(_class1, name1), GetMethod(_class2, name2), TypeComparisonMode.SignatureOnly);
-        }
+			Assert.IsTrue (MethodReferenceComparer.AreEqual (method1, method2));
+		}
 
-        static MethodDefinition GetMethod(TypeDefinition type, string name)
-        {
-            return type.Methods.Single(m => m.Name == name);
-        }
+		bool CompareSignatures (string name)
+		{
+			return CompareSignatures (name, name);
+		}
 
-        class GenericClass<T>
-        {
-        }
+		bool CompareSignatures (string name1, string name2)
+		{
+			return MethodReferenceComparer.AreSignaturesEqual (GetMethod (_class1, name1), GetMethod (_class2, name2), TypeComparisonMode.SignatureOnly);
+		}
 
-        class Class1
-        {
-            void MethodWithNoParametersOrReturn() {}
-            void GenericMethodWithNoParametersOrReturn<T>() {}
-            static void StaticMethodWithNoParametersOrReturn() {}
-            static void GenericStaticMethodWithNoParametersOrReturn<T>() {}
+		static MethodDefinition GetMethod (TypeDefinition type, string name)
+		{
+			return type.Methods.Single (m => m.Name == name);
+		}
 
-            void MethodWithIntParameterAndVoidReturn(int a) {}
+		class GenericClass<T> {
 
-            void GenericMethodWithGenericParameter<T>(T t) {}
-            void GenericMethodWithGenericParameterArray<T>(T[] t) {}
-            void GenericMethodWithByReferenceGenericParameter<T>(ref T a) {}
-            void GenericMethodWithGenericInstanceGenericParameter<T>(GenericClass<T> a) {}
-        }
+		}
 
-        class Class2
-        {
-            void MethodWithNoParametersOrReturn() {}
-            void GenericMethodWithNoParametersOrReturn<T>() {}
-            static void StaticMethodWithNoParametersOrReturn() {}
-            static void GenericStaticMethodWithNoParametersOrReturn<T>() {}
+		class Class1 {
 
-            void MethodWithIntParameterAndVoidReturn(int a) {}
+			void MethodWithNoParametersOrReturn () {}
+			void GenericMethodWithNoParametersOrReturn<T> () {}
+			static void StaticMethodWithNoParametersOrReturn () {}
+			static void GenericStaticMethodWithNoParametersOrReturn<T> () {}
 
-            void GenericMethodWithGenericParameter<T>(T t) {}
-            void GenericMethodWithGenericParameterArray<T>(T[] t) {}
-            void GenericMethodWithByReferenceGenericParameter<T>(ref T a) {}
-            void GenericMethodWithGenericInstanceGenericParameter<T>(GenericClass<T> a) {}
-        }
-    }
+			void MethodWithIntParameterAndVoidReturn (int a) {}
+
+			void GenericMethodWithGenericParameter<T> (T t) {}
+			void GenericMethodWithGenericParameterArray<T> (T[] t) {}
+			void GenericMethodWithByReferenceGenericParameter<T> (ref T a) {}
+			void GenericMethodWithGenericInstanceGenericParameter<T> (GenericClass<T> a) {}
+		}
+
+		class Class2 {
+
+			void MethodWithNoParametersOrReturn () {}
+			void GenericMethodWithNoParametersOrReturn<T> () {}
+			static void StaticMethodWithNoParametersOrReturn () {}
+			static void GenericStaticMethodWithNoParametersOrReturn<T> () {}
+
+			void MethodWithIntParameterAndVoidReturn (int a) {}
+
+			void GenericMethodWithGenericParameter<T> (T t) {}
+			void GenericMethodWithGenericParameterArray<T> (T[] t) {}
+			void GenericMethodWithByReferenceGenericParameter<T> (ref T a) {}
+			void GenericMethodWithGenericInstanceGenericParameter<T> (GenericClass<T> a) {}
+		}
+	}
 }
