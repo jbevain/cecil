61e262e8e16c14f1783f3df304c0937f7f67544f
Michael Voorhees <michaelv@unity3d.com>
2021-02-09 15:45:49 -0500

    Revert "Refactor AddLayoutInfo"
    This reverts commit 6070fe78e62b743eaed7bb9a6675f8170e4e27e6.

--- Mono.Cecil/AssemblyWriter.cs
+++ Mono.Cecil/AssemblyWriter.cs
@@ -1435,7 +1435,27 @@ namespace Mono.Cecil {
 			if (type.HasInterfaces)
 				AddInterfaces (type);
 
-			AddLayoutInfo (type);
+			if (type.HasLayoutInfo)
+				AddLayoutInfo (type);
+			else
+			if (type.IsValueType)
+			{
+				var no_instance_fields = !type.HasFields;
+				if (!no_instance_fields)
+				{
+					var fields = type.Fields;
+
+					for (int i = 0; i < fields.Count; i++)
+						if (!fields [i].IsStatic)
+						{
+							no_instance_fields = false;
+							break;
+						}
+				}
+
+				if (no_instance_fields)
+					GetTable<ClassLayoutTable> (Table.ClassLayout).AddRow (new ClassLayoutRow (0, 1, type.token.RID));
+			}
 
 			if (type.HasFields)
 				AddFields (type);
@@ -1554,36 +1574,12 @@ namespace Mono.Cecil {
 
 		void AddLayoutInfo (TypeDefinition type)
 		{
-			if (type.HasLayoutInfo) {
-				var table = GetTable<ClassLayoutTable> (Table.ClassLayout);
+			var table = GetTable<ClassLayoutTable> (Table.ClassLayout);
 
-				table.AddRow (new ClassLayoutRow (
-					(ushort) type.PackingSize,
-					(uint) type.ClassSize,
-					type.token.RID));
-
-				return;
-			}
-
-			if (type.IsValueType && HasNoInstanceField (type)) {
-				var table = GetTable<ClassLayoutTable> (Table.ClassLayout);
-
-				table.AddRow (new ClassLayoutRow (0, 1, type.token.RID));
-			}
-		}
-
-		static bool HasNoInstanceField (TypeDefinition type)
-		{
-			if (!type.HasFields)
-				return true;
-
-			var fields = type.Fields;
-
-			for (int i = 0; i < fields.Count; i++)
-				if (!fields [i].IsStatic)
-					return false;
-
-			return true;
+			table.AddRow (new ClassLayoutRow (
+				(ushort) type.PackingSize,
+				(uint) type.ClassSize,
+				type.token.RID));
 		}
 
 		void AddNestedTypes (TypeDefinition type)
