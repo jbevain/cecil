f4257c95e68deb20c54bbe0aa7763d576d3e3fec
SteveGilham <SteveGilham@users.noreply.github.com>
2024-03-23 13:21:52 +0000

    Choose FIPS compliant implementation of the SHA1 algorithm

--- Mono.Security.Cryptography/CryptoService.cs
+++ Mono.Security.Cryptography/CryptoService.cs
@@ -93,7 +93,7 @@ namespace Mono.Cecil {
 				+ (strong_name_directory.VirtualAddress - text.VirtualAddress));
 			var strong_name_length = (int) strong_name_directory.Size;
 
-			var sha1 = new SHA1Managed ();
+			var sha1 = new SHA1CryptoServiceProvider ();
 			var buffer = new byte [buffer_size];
 			using (var crypto_stream = new CryptoStream (Stream.Null, sha1, CryptoStreamMode.Write)) {
 				stream.Seek (0, SeekOrigin.Begin);
@@ -131,7 +131,7 @@ namespace Mono.Cecil {
 		{
 			const int buffer_size = 8192;
 
-			var sha1 = new SHA1Managed ();
+			var sha1 = new SHA1CryptoServiceProvider ();
 			var buffer = new byte [buffer_size];
 
 			using (var crypto_stream = new CryptoStream (Stream.Null, sha1, CryptoStreamMode.Write))
@@ -142,7 +142,7 @@ namespace Mono.Cecil {
 
 		public static byte [] ComputeHash (params ByteBuffer [] buffers)
 		{
-			var sha1 = new SHA1Managed ();
+			var sha1 = new SHA1CryptoServiceProvider ();
 
 			using (var crypto_stream = new CryptoStream (Stream.Null, sha1, CryptoStreamMode.Write)) {
 				for (int i = 0; i < buffers.Length; i++) {
