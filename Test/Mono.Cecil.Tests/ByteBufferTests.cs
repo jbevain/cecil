f697188b377b4e89296162cb8bb62edde0311ed9
Simon Ferquel <simon.ferquel@unity3d.com>
2024-08-22 14:22:53 +0200

    Fix WriteCompressedInt32 logic by using the same code as in System.Reflection.Metadata
    Note: the implementation in SRM uses methods that are not available in netfx 4.0. To make the code work with all platforms supported by Mono.Cecil, I extracted the required methods.

---
+++ Test/Mono.Cecil.Tests/ByteBufferTests.cs
@@ -0,0 +1,16 @@
+﻿using Mono.Cecil.PE;
+using NUnit.Framework;
+
+namespace Mono.Cecil.Tests {
+	public class ByteBufferTests {
+		[Test]
+		public void TestLargeIntegerCompressed ()
+		{
+			var testee = new ByteBuffer ();
+			testee.WriteCompressedInt32 (-9076);
+			testee.position = 0;
+			var result = testee.ReadCompressedInt32 ();
+			Assert.AreEqual (-9076, result);
+		}
+	}
+}
